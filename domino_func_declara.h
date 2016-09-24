453c91fbaae60e034e85a920a9479d47b755ca9c
kadu753 <kadu.grando@hotmail.com>
2016-09-24 17:24:10 -0300

    Update domino_func_declara.h

--- domino_func_declara.h
+++ domino_func_declara.h
@@ -8,6 +8,12 @@ typedef struct _pecaDomino{
 	struct _pecaDomino *left;
 }TppecaDomino;
 
+typedef struct _asciiart{
+	int indice;
+	char desenho[5][22];
+	struct _asciiart *prox;
+}Asciiart;
+
 TppecaDomino *inicializa();
 TppecaDomino *cria_pecas();
 TppecaDomino *insere_inicio(TppecaDomino *l, int num1, int num2);
@@ -17,6 +23,12 @@ TppecaDomino *copiar(TppecaDomino *l, int num);
 int valida_jogada(TppecaDomino *mesa, TppecaDomino *jogada);
 TppecaDomino *insere_mesa(TppecaDomino *mesa, TppecaDomino *jogada, int lado);
 TppecaDomino *remove_peca_jogada(TppecaDomino *mao, TppecaDomino *jogada);
+Asciiart *inicializaasc();
+void desenhar(Asciiart *peca, int valor, int x, int fazer);
+Asciiart *alocardesenho(Asciiart *peca);
+Asciiart *chamardesenho(TppecaDomino *l);
+int contar_elementosasc(Asciiart *l);
+void printar(Asciiart *peca);
 int testa_pecas(TppecaDomino *l, TppecaDomino *mesa);
 void imprime(TppecaDomino *l);
 void imprime_info(TppecaDomino *mesa, TppecaDomino *mao_jogador, TppecaDomino *mao_bot, TppecaDomino *monte);
