90488aca593a1389f800b74282d5a226c1b94fbe
arturspon <artursponchi@gmail.com>
2016-09-25 22:12:29 -0300

    Update domino_art_bot.c

--- domino_art_bot.c
+++ domino_art_bot.c
@@ -11,9 +11,9 @@
 #include "domino_func_declara.h"
 
 int main(){
-	TppecaDomino *monte, *jogador = NULL, *aux, *bot = NULL, *mesa = NULL, *pecajogada;
+	TppecaDomino *monte, *jogador = NULL, *aux, *bot = NULL, *mesa = NULL, *pecajogada, *aux_mao_bot;
 	Asciiart *desenho_jogador = inicializaasc(), *desenho_mesa = inicializaasc();
-	int i, cont = 0, cont2 = 0, escolha_lado, numero_pecas, tam, random;
+	int i, cont = 0, cont2 = 0, escolha_lado, numero_pecas, tam, random, flag_jogador = 1, jogo_ativo = 1, flag_lado = 1;
 	monte = cria_pecas();
 	srand(clock());
 	
@@ -35,16 +35,14 @@ int main(){
 	aux = copiar(monte, i);
 	mesa = insere_fim(mesa, aux->numberLeft, aux->numberRight);
 	monte = excluir(monte, aux->numberLeft, aux->numberRight);
-
-	TppecaDomino *aux_mao_bot = bot;
-	system("reset");
+	aux_mao_bot = bot;
+	system("clear");
 	logo_uffs();
-	getchar();
+	sleep(1);
+
 	//Interatividade do jogador
-	int flag_jogo = 1;
-	int flag_jogador = 1;
-	while(flag_jogo){
-		while(flag_jogador){
+	do{
+		while(flag_jogador == 1){
 			imprime_info(mesa, jogador, bot, monte);
 			printf("Mesa\n");
 			desenho_mesa = chamardesenho(mesa);
@@ -54,29 +52,18 @@ int main(){
 			printar(desenho_jogador);
 			int pecaescolhida;
 			numero_pecas = contar_elementos(jogador);
-			if(numero_pecas == 0){
-				quem_ganhou(jogador, bot, monte);
-				flag_jogo = 0;
-				break;
-			}else if(contar_elementos(bot) == 0){
-				quem_ganhou(jogador, bot, monte);
-				flag_jogo = 0;
-				break;
-			}else if((testa_pecas(jogador, mesa) == 0 || testa_pecas(bot, mesa) == 0) && monte == NULL){
-				quem_ganhou(jogador, bot, monte);
-				flag_jogo = 0;
-				break;
-			}
 			printf("Informe o índice da peça que deseja jogar ou 0 para pescar: ");
-			while(pecaescolhida != 1){ // Só deixa o jogador inserir números inteiros.
+			while(scanf("%d", &pecaescolhida) != 1){ // Só deixa o jogador inserir números inteiros.
 				scanf("%d", &pecaescolhida);
 				getchar();
-			}				
-			if(pecaescolhida >= 1 && pecaescolhida <= numero_pecas && numero_pecas != 0){
+			}		
+			if(pecaescolhida >= 1 && pecaescolhida <= numero_pecas && numero_pecas != 0){ // Confere se o índice passado pelo jogador é válido.
 				pecajogada = jogador;
-				for(i = 1; i<pecaescolhida; i++){ pecajogada = pecajogada->right; }
-				pecaescolhida = valida_jogada(mesa, pecajogada);
-				if(pecaescolhida == 1){
+				for(i = 1; i<pecaescolhida; i++){ // Encontra a peça escolhida pelo jogador.
+					pecajogada = pecajogada->right;
+				}
+				pecaescolhida = valida_jogada(mesa, pecajogada); // Chama a função para validar ou não a jogada.
+				if(pecaescolhida == 1){ // Se a jogada for válida em apenas um dos lados da mesa.
 					mesa = insere_mesa(mesa, pecajogada, 0);
 					jogador = remove_peca_jogada(jogador, pecajogada);
 					imprime_info(mesa, jogador, bot, monte);
@@ -87,8 +74,7 @@ int main(){
 					desenho_jogador = chamardesenho(jogador);
 					printar(desenho_jogador);
 					flag_jogador = 0;
-				}else if(pecaescolhida == 2){
-					int flag_lado = 1;
+				}else if(pecaescolhida == 2){ // Se a jogada for válida em ambos os lados da mesa.
 					while(flag_lado){
 						printf("Em qual lado deseja inserir?\n1 = Esquerdo | 2 = Direito\n");
 						scanf("%d", &escolha_lado);
@@ -133,8 +119,11 @@ int main(){
 						flag_jogador = 0;
 					}			
 				}
-			}else if(pecaescolhida == 0){
-				if(contar_elementos(monte) > 0){
+			}else if(pecaescolhida == 0){ // Código para o jogador pescar.
+				if(valida_jogada(mesa, jogador) == 0 && monte == NULL){
+						jogo_ativo = 0;
+					}
+				if(contar_elementos(monte) > 0){ // Verifica se ainda há peças para pescar.
 					int pesca_escolha = 0;
 					do{
 						system("clear");
@@ -172,36 +161,53 @@ int main(){
 					system("clear");
 					printf("Erro. Não há mais peças para pescar.\n\n");
 					sleep(1);
+					if(valida_jogada(mesa, jogador) == 0 && monte == NULL){
+						jogo_ativo = 0;
+					}
 				}		
 			} else {
 				printf("\nErro. Insira um índice entre 1 e %d\n", numero_pecas);
 				sleep(2);				
 			}			
 		} // fecha while flag_jogador
+		if(jogador == NULL){
+			break;
+		}
 		//VEZ DO BOT JOGAR
 		aux_mao_bot = bot;
 		printf("\nBOT JOGANDO...\n\n");
-		sleep(1);
+		sleep(1.5);
 		while(valida_jogada(mesa, aux_mao_bot) == 0 && aux_mao_bot->right != NULL){
 			aux_mao_bot = aux_mao_bot->right;				
 		}
-		while(monte != NULL){
-			if(aux_mao_bot->right == NULL && valida_jogada(mesa, aux_mao_bot) == 0){
-				printf("BOT PESCANDO...\n");
-				tam = contar_elementos(monte);
-				random = abs(rand()%(tam));
-				aux = copiar(monte, random);
-				bot = insere_fim(bot, aux->numberLeft, aux->numberRight);
-				monte = excluir(monte, aux->numberLeft, aux->numberRight);
-				aux_mao_bot = bot;				
-			}
-			break;
+		
+		while(valida_jogada(mesa, aux_mao_bot) == 0 && monte != NULL){
+			printf("BOT PESCANDO...\n");
+			tam = contar_elementos(monte);
+			random = abs(rand()%(tam));
+			aux = copiar(monte, random);
+			bot = insere_fim(bot, aux->numberLeft, aux->numberRight);
+			monte = excluir(monte, aux->numberLeft, aux->numberRight);
+			aux_mao_bot = aux_mao_bot->right;			
+		}
+		if(valida_jogada(mesa, aux_mao_bot) != 0){
+			aux_mao_bot = bot;
+		}
+		
+		while(valida_jogada(mesa, aux_mao_bot) == 0 && aux_mao_bot->right != NULL){
+			aux_mao_bot = aux_mao_bot->right;				
 		}
 		if(valida_jogada(mesa, aux_mao_bot) != 0){				
 			mesa = insere_mesa(mesa, aux_mao_bot, 0);
 			bot = remove_peca_jogada(bot, aux_mao_bot);			
 		}
+		if(bot == NULL){
+			break;
+		}
 		flag_jogador = 1;
-	} // fecha while flag_jogo
+	}while(jogo_ativo); // fecha while flag_jogo
+	quem_ganhou(jogador, bot, monte);	
 	return 0;
 }
+
+//while((contar_elementos(jogador) != 0 || contar_elementos(bot) != 0 || testa_pecas(jogador, mesa) == 0 || testa_pecas(bot, mesa) == 0) && monte != NULL);
